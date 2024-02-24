70f83aecd4d96a35f0847feb17f3aa62c854740f
Hunde-D <hunddh2025@gmail.com>
2024-02-24 18:25:33 +0300

    add react blog-card
    frontend-mentor blog-preview-card challenge solution using react

---
+++ blog-card/src/App.jsx
@@ -0,0 +1,12 @@
+import Panel from './Components/Panel';
+import Body from './Components/Body';
+import './App.css';
+export default function App(){
+    return (
+        <div className='blog-card'>
+            <Panel/>
+            <Body/>
+        </div>
+
+    );
+}
---
+++ blog-card/src/Components/Panel.jsx
@@ -0,0 +1,5 @@
+import img from '../assets/images/illustration-article.svg';
+
+const Panel = () => <img src={img} className='card-img' alt='panel image'/>;
+
+export default Panel;
